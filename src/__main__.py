ef835427edf2f33f378de47b7da4e24b7d6cae9a
Jordan Mackie <jm354@st-andrews.ac.uk>
2019-04-13 20:34:29 +0100

    Added structure image

--- src/__main__.py
+++ src/__main__.py
@@ -23,11 +23,12 @@ def parse_args() -> Dict:
 
 
 def load_data(input_file, output_file, keys_file):
+    """Load input, output, and class titles from the given file names"""
     # Read in input data
-    inputs = pd.read_csv(input_file, delimiter=',')
+    inputs = pd.read_csv(input_file, delimiter=',', error_bad_lines=True, dtype=float, header=None)
 
     # Read in output data
-    outputs = pd.read_csv(output_file, delimiter=',')
+    outputs = pd.read_csv(output_file, delimiter=',', error_bad_lines=True, dtype=float, header=None)
 
     with open(keys_file) as f:
         keys = json.load(f)
@@ -35,13 +36,20 @@ def load_data(input_file, output_file, keys_file):
     return inputs, outputs, keys
 
 
-def split_data(inputs, outputs):
-    return train_test_split(inputs, outputs, test_size=0.2, random_state=42,
-                            stratify=outputs)
+def split_data(input_df, output_df):
+    """Split input and outputs using stratification with a 80%-20% split"""
+    return train_test_split(input_df, output_df, test_size=0.2, random_state=42,
+                            stratify=output_df)
+
+
+def clean_data(data_to_clean):
+    """Removes any rows with empty or missing values"""
+    data_to_clean.dropna(how='all', inplace=True)
 
 
 if __name__ == "__main__":
     args = parse_args()
     inputs, outputs, keys = load_data(args[INPUTS_ARG], args[OUTPUTS_ARG], args[KEYS_ARG])
+    clean_data(inputs)
+    clean_data(outputs)
     x_train, x_test, y_train, y_test = split_data(inputs, outputs)
-
