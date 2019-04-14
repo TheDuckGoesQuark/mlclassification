baaa2a0ddf76dc9e4f25cf5d36a75599e6f42ce9
Jordan Mackie <jm354@st-andrews.ac.uk>
2019-04-14 19:25:05 +0100

    Add plots of each row in classes

--- src/__main__.py
+++ src/__main__.py
@@ -2,6 +2,9 @@ from typing import Dict, Tuple
 
 import pandas as pd
 import json
+import wave
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
 from argparse import ArgumentParser
@@ -25,15 +28,18 @@ def parse_args() -> Dict:
 def load_data(input_file, output_file, keys_file):
     """Load input, output, and class titles from the given file names"""
     # Read in input data
-    inputs = pd.read_csv(input_file, delimiter=',', error_bad_lines=True, dtype=float, header=None)
+    input_data = pd.read_csv(input_file, delimiter=',', error_bad_lines=True, dtype=float, header=None)
 
     # Read in output data
-    outputs = pd.read_csv(output_file, delimiter=',', error_bad_lines=True, dtype=float, header=None)
+    output_data = pd.read_csv(output_file, delimiter=',', error_bad_lines=True, dtype=float, header=None)
 
+    class_keys = {}
     with open(keys_file) as f:
-        keys = json.load(f)
+        class_keys = json.load(f)
 
-    return inputs, outputs, keys
+    class_keys = {int(key): value for key, value in class_keys.items()}
+
+    return input_data, output_data, class_keys
 
 
 def split_data(input_df, output_df):
@@ -46,10 +52,64 @@ def clean_data(input_df, output_df):
     """Removes any rows with empty or missing values"""
     all_rows = pd.concat([input_df, output_df], axis=1)
     all_rows.dropna(how='all', inplace=True)
-    # TODO split all rows back to input cols and output col
-    input_df = all_rows[:, :all_rows.shape[1]]
-    output_df = all_rows[:, all_rows.shape[1]:]
+    input_df = all_rows.iloc[:, 0:input_df.shape[1]]
+    output_df = all_rows.iloc[:, input_df.shape[1]:all_rows.shape[1]]
     return input_df, output_df
+
+
+def plot_class_scatter(rows_of_class, class_name, max_value, min_value):
+    """Plot scatter graphs for the functions applied to the first channel"""
+    fig = plt.figure(figsize=(30, 5))
+    fig.suptitle("Components for class {}".format(class_name))
+    function_to_channel_plots = {function_name: [] for function_name in ["Mean", "Min", "Max"]}
+    n_plots = 1
+    # For each function
+    for function_idx, function_name in enumerate(function_to_channel_plots):
+        # For each channel
+        for channel_idx in range(0, 4):
+            plot = fig.add_subplot(1, 14, n_plots + function_idx)
+            channel_number = ((n_plots - 1) % 4) + 1
+            plot.set_title("{} of Channel {}".format(function_name, channel_number))
+            plot.set_xlabel("Components")
+            # Only need title for first graph for each function
+            if channel_idx == 0:
+                plot.set_ylabel("{} of 100 pulses".format(function_name))
+
+            plot.set_ylim((min_value, max_value))
+            function_to_channel_plots[function_name].append(plot)
+            n_plots += 1
+
+    components_per_function = 256
+    components_per_channel = 64
+    for index, row in rows_of_class.iterrows():
+        for function_idx, (function, channel_plots) in enumerate(function_to_channel_plots.items()):
+            for channel_idx, channel_plot in enumerate(channel_plots):
+                x = np.arange(0, components_per_channel)
+                start = (function_idx * components_per_function) + (channel_idx * components_per_channel)
+                end = start + components_per_channel
+                y = row[start:end]
+                channel_plot.scatter(x, y)
+
+    plt.savefig("{}.png".format(class_name))
+
+
+def visualise_signal(input_rows_for_class, output_rows_for_class, class_keys):
+    all_rows = pd.merge(input_rows_for_class, output_rows_for_class, left_index=True, right_index=True)
+    classes = all_rows.iloc[:, -1].unique()
+    max_value = input_rows_for_class.values.max()
+    min_value = input_rows_for_class.values.min()
+    for class_enum in classes:
+        # Get indices of all rows for that class
+        indices_of_class = all_rows.index[all_rows.iloc[:, -1] == class_enum]
+        # Get rows using those indices
+        class_rows = all_rows.loc[indices_of_class]
+        # Plot components for each class
+        plot_class_scatter(class_rows, class_keys[class_enum], max_value, min_value)
+
+
+def visualise_class_distribution(y_vals, y_keys):
+    fig, ax = plt.subplots()
+    y_vals.value_counts().plot(ax=ax, kind='bar')
 
 
 if __name__ == "__main__":
@@ -57,3 +117,5 @@ if __name__ == "__main__":
     inputs, outputs, keys = load_data(args[INPUTS_ARG], args[OUTPUTS_ARG], args[KEYS_ARG])
     inputs, outputs = clean_data(inputs, outputs)
     x_train, x_test, y_train, y_test = split_data(inputs, outputs)
+    # visualise_signal(x_train, y_train, keys)
+    visualise_class_distribution(y_train, keys)
